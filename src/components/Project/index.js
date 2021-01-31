55282aec1ff1c1bda25e7519004fa3649f8c641c
claire p <cpuckett9@gmail.com>
2021-01-30 22:03:38 -0600

    build

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid'
 import {CardMedia} from "@material-ui/core";
-import CardActionArea from '@material-ui/core/CardActionArea';
+// import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import { makeStyles } from '@material-ui/core/styles';
